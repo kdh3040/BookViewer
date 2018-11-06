e42c08e9bf6f13320689b062b24f57e77524342e
kdh3040 <kdh30400@naver.com>
2018-11-06 15:16:57 +0900

    [김도형] 뷰어 스크롤바 작업중

--- app/src/main/java/bookviewer/bookviewer/com/bookviewer/ViewActivity.java
+++ app/src/main/java/bookviewer/bookviewer/com/bookviewer/ViewActivity.java
@@ -3,19 +3,24 @@ package bookviewer.bookviewer.com.bookviewer;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 import com.github.barteksc.pdfviewer.PDFView;
 import com.github.barteksc.pdfviewer.listener.OnPageChangeListener;
+import com.github.barteksc.pdfviewer.listener.OnPageScrollListener;
+import com.github.barteksc.pdfviewer.scroll.DefaultScrollHandle;
+import com.github.barteksc.pdfviewer.scroll.ScrollHandle;
 import com.github.barteksc.pdfviewer.util.FitPolicy;
 
 public class ViewActivity extends AppCompatActivity implements OnPageChangeListener {
     private  String BookPath, BookName;
     private  PDFView pdfView;
 
-    private TextView Title_Text;
+    private TextView Title_Text, Title_Page, Menu_scroll;
     private Button Title_Button;
 
     @Override
@@ -33,6 +38,9 @@ public class ViewActivity extends AppCompatActivity implements OnPageChangeListe
         Title_Button = (Button)findViewById(R.id.title_back);
         Title_Button.setOnClickListener(mClickListener);
 
+        Title_Page = (TextView)findViewById(R.id.title_page);
+
+
         pdfView = (PDFView)findViewById(R.id.pdfView);
         pdfView.fromAsset("Data/"+BookPath)
                 .enableSwipe(true)
@@ -42,12 +50,15 @@ public class ViewActivity extends AppCompatActivity implements OnPageChangeListe
                 .autoSpacing(true)
                 .pageFling(true)
                 .pageFitPolicy(FitPolicy.BOTH)
+                .scrollHandle(new DefaultScrollHandle(this))
                 .load();
     }
 
+
     @Override
     public void onPageChanged(int page, int pageCount) {
-
+        Title_Page.setText((String.format("%s / %s", page + 1, pageCount)));
+        pdfView.setScrollBarFadeDuration(1);
     }
 
     Button.OnClickListener mClickListener = new View.OnClickListener() {
@@ -55,4 +66,5 @@ public class ViewActivity extends AppCompatActivity implements OnPageChangeListe
             onBackPressed();
         }
     };
+
 }
