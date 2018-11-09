90a165c8344fade8a54b64dc681ad8d3f31b47f9
kdh3040 <kdh30400@naver.com>
2018-11-09 16:12:33 +0900

    [김도형] 회원가입 페이지로 이동

--- app/src/main/java/bookviewer/bookviewer/com/bookviewer/LoginActivity.java
+++ app/src/main/java/bookviewer/bookviewer/com/bookviewer/LoginActivity.java
@@ -1,5 +1,6 @@
 package bookviewer.bookviewer.com.bookviewer;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,6 +10,9 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Intent intent = new Intent(LoginActivity.this, SignUpActivity.class);
+        startActivity(intent);
 
 
     }
