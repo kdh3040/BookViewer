8e6f82e016dcd1cb8e23a8a63b837cb877bbb673
kdh3040 <kdh30400@naver.com>
2018-11-13 13:47:48 +0900

    [김도형] 커리큘럼 페이지 추가

---
+++ app/src/main/java/bookviewer/bookviewer/com/bookviewer/Curriculum/Curriculum_first.java
@@ -0,0 +1,48 @@
+package bookviewer.bookviewer.com.bookviewer.Curriculum;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import bookviewer.bookviewer.com.bookviewer.BookListAdapter;
+import bookviewer.bookviewer.com.bookviewer.MainActivity;
+import bookviewer.bookviewer.com.bookviewer.R;
+
+public class Curriculum_first extends Fragment {
+
+    RecyclerView BookRecyclerView;
+    BookListAdapter BookListViewAdapter;
+    View fragView;
+
+    public Curriculum_first() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        fragView = inflater.inflate(R.layout.fragment_curriculum_first,container,false);
+
+        BookRecyclerView = (RecyclerView)fragView.findViewById(R.id.recyclerview_booklist);
+        BookRecyclerView.setHasFixedSize(true);
+        BookRecyclerView.setLayoutManager(new GridLayoutManager(getContext(), 3));
+
+        BookListViewAdapter = new BookListAdapter(getContext());
+        BookRecyclerView.setAdapter(BookListViewAdapter);
+
+        return fragView;
+    }
+
+}
